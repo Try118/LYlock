bfc09a1bb67de1eaf19038193db52a9cc957574a
Try118 <1021996158@qq.com>
2018-04-17 10:19:06 +0800

    提交人：姚喜 内容：在LoginInputPassword里保存了用户的账号和密码、SharePreferences实现

--- app/src/main/java/com/diko/project/View/LoginInputPassword.java
+++ app/src/main/java/com/diko/project/View/LoginInputPassword.java
@@ -1,6 +1,7 @@
 package com.diko.project.View;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -97,9 +98,16 @@ public class LoginInputPassword extends BaseActivity {
 //                lv_announcements.setAdapter(mAdapter);
 //                com.diko.project.Module.Login login = (com.diko.project.Module.Login) success;
 
+                //保存用户的账号密码
+                SharedPreferences.Editor editor = getSharedPreferences("UserInformation", MODE_PRIVATE).edit();
+                editor.putString("account",account);
+                editor.putString("password",password);
+                editor.apply();
+
                 Intent i = new Intent(LoginInputPassword.this,AddLock.class);
                 i.putExtra("phone",account);
                 i.putExtra("password",password);
+
                 startActivity(i);
             }
 
