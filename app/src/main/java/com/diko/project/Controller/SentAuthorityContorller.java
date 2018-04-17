c31edea26c5136645bed59e2c6f2bbe3d15f0f19
ljtlcj <1521826348@qq.com>
2018-04-17 19:37:56 +0800

    提交人：林楚杰         内容: 修改名称

--- app/src/main/java/com/diko/project/Controller/SentAuthorityContorller.java
+++ app/src/main/java/com/diko/project/Controller/SentAuthorityContorller.java
@@ -1,9 +1,63 @@
 package com.diko.project.Controller;
 
+import android.util.Log;
+
+import com.diko.project.Manager.InterfaceManger;
+import com.diko.project.Utils.RetrofitUtils;
+
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 /**
  * Created by jie on 2018/4/14.
  */
 
 public class SentAuthorityContorller {
+    /**
+     * 门锁信息列表模块
+     */
+    public static void ReadAllLock(Map<String, RequestBody> map, List<MultipartBody.Part> parts, final InterfaceManger.OnRequestListener listener) {
+        Call<ResponseBody> call = RetrofitUtils.getInstance().ReadAllLock(map, parts);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (listener == null) {
+                    return;
+                }
+                if (!response.isSuccessful() || response == null) {
+                    listener.onError("服务器错误，error code:" + response.code());
+                    return;
+                }
+                try {
+                    String body = response.body().string().toString();
+                    Object object = body;
+                    if (!body.contains("error")) {
+                        listener.onSuccess(object);
+                    } else {
+                        listener.onError("未知错误");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                listener.onComplete();
+            }
 
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                if (listener == null) {
+                    return;
+                }
+                Log.e("onFailure", t.toString());
+                listener.onError(t.toString());
+                listener.onComplete();
+            }
+        });
+    }
 }
--- app/src/main/java/com/diko/project/View/LockSentAuthorityOne.java
+++ app/src/main/java/com/diko/project/View/LockSentAuthorityOne.java
@@ -1,7 +1,10 @@
 package com.diko.project.View;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.diko.basemodule.Essential.BaseTemplate.BaseActivity;
@@ -11,12 +14,13 @@ import com.diko.project.R;
  * Created by jie on 2018/4/12.
  */
 
-public class LockSentAuthorityOne extends BaseActivity {
+public class LockSentAuthorityOne extends BaseActivity implements TextWatcher {
     private TextView back;//返回控件
     private EditText phone;//填入电话号码
     private TextView get_phone;//从通讯录获取联系人信息
     private TextView next;//下一步
     private TextView Wechat;//wechat联系人
+    private LinearLayout name_line;//被隐藏起来的控件，其实我不想这么做的，希望有后来者，以后修改
     @Override
     public int getLayoutId() {
         return R.layout.activity_sent_lock_authority_one;
@@ -29,6 +33,7 @@ public class LockSentAuthorityOne extends BaseActivity {
         get_phone = findView(R.id.get_phone);
         next = findView(R.id.next);
         Wechat = findView(R.id.WeChat);
+        name_line = findView(R.id.name_line);
     }
 
     @Override
@@ -37,6 +42,7 @@ public class LockSentAuthorityOne extends BaseActivity {
         get_phone.setOnClickListener(this);
         next.setOnClickListener(this);
         Wechat.setOnClickListener(this);
+        phone.addTextChangedListener(this);
     }
 
     @Override
@@ -62,5 +68,20 @@ public class LockSentAuthorityOne extends BaseActivity {
             default:
                 break;
         }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        name_line.setVisibility(View.VISIBLE);
     }
 }
